b97adad9a6cea55d91cc1e09c7c87508c58ef595
Gustaa421 <gustaas737@gmail.com>
2024-08-09 13:51:20 -0300

    meu curriculo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,43 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  pessoa = {
+    foto: 'https://img.freepik.com/fotos-gratis/macaco-de-vista-frontal-usando-oculos-escuros_23-2150754441.jpg',
+    nome: 'Pombinha da Paz Silva',
+    objetivo: 'Programador HTML & CSS',
+    contato: {
+      email: 'pombinha@hotmail.com',
+      telefone: '(11) 99333333',
+      github: 'github.com/pombinha',
+      linkedin: 'linkedim.com/pombinha'
+    },
+    softskills: [
+      'Comunicação',
+      'Proatividade',
+      'Trabalho em Grupo'
+    ],
+    formacao: [
+      {
+        ano_inicio: '2022',
+        ano_fim: '2024',
+        instituicao: 'Etec Sales Gomes',
+        curso: "Técnico em Desenvolvimento de Sistemas"
+     },
+     {
+        ano_inicio: '2025',
+        ano_fim: '2027',
+        instituicao: 'Faculdade de Tecnologia - Fatec',
+        curso: "Superior em Análise e Desenvolvimento de Sistemas"
+     }
+    ],
+    projetos: [
+      {
+        ano: '2023',
+        instituicao: 'Etec Sales Gomes',
+        nome_projeto: 'Etec de Portas Abertas',
+        descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores neque suscipit dolorem dolorum perspiciatis maxime cupiditate quas laboriosam dignissimos quisquam? Quidem dolor omnis molestias dolorum rem cumque perspiciatis, at qui?'
+      }
+    ]
+  }
   constructor(){ }
 }
